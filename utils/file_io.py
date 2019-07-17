f07c9a8d4fcbe8f6bc64e08a680f0cf97a211044
Anthony Keating <anthonyk1225@gmail.com>
2019-07-17 13:15:11 -0400

    added error handling for writing rows to file

--- utils/file_io.py
+++ utils/file_io.py
@@ -32,17 +32,21 @@ def write_column_headers(workbook, worksheet, column_headers):
 
 def write_worksheet_rows(workbook, worksheet, selected_keys, data, row, col):
   for item in selected_keys:
-    cell_type, name, formatting = item['cell_type'], item['name'], item['formatting']
-    value = data[name]
-    write_types = {
-      'number': worksheet.write_number,
-      'date': worksheet.write_datetime,
-      'string': worksheet.write_string,
-    }
-    write_types[cell_type](
-      row,
-      col,
-      float(value) if cell_type is 'number' else value,
-      workbook.add_format(formatting)
-    )
+    try:
+      cell_type, name, formatting = item['cell_type'], item['name'], item['formatting']
+      value = data[name]
+      write_types = {
+        'number': worksheet.write_number,
+        'date': worksheet.write_datetime,
+        'string': worksheet.write_string,
+      }
+      write_types[cell_type](
+        row,
+        col,
+        float(value) if cell_type is 'number' else value,
+        workbook.add_format(formatting)
+      )
+    except Exception as e:
+      print("Had trouble writing an item to the cell", str(e))
+      worksheet.write(row, col, '')
     col += 1
