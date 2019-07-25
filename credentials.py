56c30c66a079314d7a3b69dc37de08fa58d60a71
Anthony Keating <anthonyk1225@gmail.com>
2019-07-25 01:16:19 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# your credentials
+username = "" # Your robinhood username or email
+password = "" # Your robinhood password
+
+# should also be generated, but needs to be found for now
+device_token = "" # Your device token in the headers
