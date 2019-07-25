d56d4e0c7980b6bc249de8bd500d6f21d58b88a3
Anthony Keating <anthonyk1225@gmail.com>
2019-07-25 11:57:32 -0400

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-# your credentials
-username = "" # Your robinhood username or email
-password = "" # Your robinhood password
-
-# should also be generated, but needs to be found for now
-device_token = "" # Your device token in the headers
