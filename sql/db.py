bd3c4f9f74a9a876ebf4bf01be6737a700a65f9c
Anthony Keating <anthonyk1225@gmail.com>
2020-09-21 22:29:27 -0400

    added sql dump that runs automatically when running an entity

--- sql/db.py
+++ sql/db.py
@@ -1,15 +1,28 @@
-import os
+import os, sqlite3
 from sql.tables import instruments, authorization, device_token, splits
 
 def run():
   # if 'robinhood.db' in os.listdir('./'):
   #   return False
 
+  try:
+    with open("sql/dumps/splits.sql", "r") as sql_file:
+      sql_script = sql_file.read()
+
+    db = sqlite3.connect("robinhood.db")
+    cursor = db.cursor()
+    cursor.executescript(sql_script)
+    db.commit()
+    db.close()
+  except Exception as e:
+    print("Splits table already created")
+    # print("Couldn't add the splits table.", e)
+
   tables = [
     instruments.create_tables,
     authorization.create_tables,
     device_token.create_tables,
-    splits.create_tables
+    # splits.create_tables
   ]
 
   for table in tables:
